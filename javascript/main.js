40317beb8d2b2ba80c9200da78b785a1f9ebff70
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-14 15:30:29 +0200

    Struttura basic

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,40 @@
-console.log('JS OK') ;
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65
+
+*/
+
+
+// Get data user
+
+var numberKm = prompt('Inserisci il numero di Km che vuoi percorrere');
+var age = prompt('Inserisci la tua età');
+//console.log('km ' + numberKm + 'età ' + age);
+
+// Prezzo biglietto base
+
+var ticketPrice = numberKm * 0.21;
+
+document.getElementById('ticket-price-base').innerHTML = 'Il costo del biglietto è: ' + ticketPrice.toFixed(2);
+//console.log(ticketPrice);
+
+// Sconti in base all'età
+
+if (age < 18) {
+  ticketPrice =  ticketPrice - (ticketPrice * 20 / 100);
+  document.getElementById('ticket-price-discount').innerHTML = 'Il tuo biglietto è scontato del 20%: ' + ticketPrice.toFixed(2);
+
+  //console.log(ticketPrice);
+}
+else if (age > 65) {
+  ticketPrice =  ticketPrice - (ticketPrice * 40 / 100);
+  document.getElementById('ticket-price-discount').innerHTML = 'Il tuo biglietto è scontato del 40%: ' + ticketPrice.toFixed(2);
+
+  //console.log(ticketPrice);
+}
+
+//ticketPrice = ticketPrice.toFixed(2);
+  //console.log(ticketPrice);
